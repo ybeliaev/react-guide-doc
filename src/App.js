45f4454e0e995f54d67c4b5aaceeb350c9c3674b
Yuran <ybeliaev@gmail.com>
2020-06-10 16:33:59 +0300

    sdf

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+import Element from "./components/element";
 
 function App() {
   return (
     <div className="App">
+      <Element handleClick={() => console.log(Element.props)} />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Learn React
       </header>
     </div>
   );
