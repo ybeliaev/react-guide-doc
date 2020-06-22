e7ee802a2c724ecd62d0d6e44321b58ca5945023
Yuran <ybeliaev@gmail.com>
2020-06-22 12:48:59 +0300

    add structure FilteredProductTable

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Button from "./components/Button";
 import LoginControl from "./components/Login/LoginControl";
 import NameForm from "./components/Form/NameForm";
 import TemperatureInput from "./components/Temperature/CalcTemperature";
+import FilteredProductTable from "./components/ProductTable/FilteredProductTable";
 
 function handleClick(event) {
   event.preventDefault();
@@ -23,9 +24,8 @@ function App() {
       <header className="App-header">
         <Button />
         <LoginControl />
-
         <TemperatureInput />
-
+        <FilteredProductTable />
         <Ticker />
       </header>
     </div>
