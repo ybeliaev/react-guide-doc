c76872d32c0719bde6c0c8287fd57c6fe512141f
Yuran <ybeliaev@gmail.com>
2020-06-22 10:57:37 +0300

    any style farenheite

--- src/components/Temperature/CalcTemperature.js
+++ src/components/Temperature/CalcTemperature.js
@@ -11,12 +11,12 @@ function toFahrenheit(celsius) {
   return (celsius * 9) / 5 + 32;
 }
 
-function tryConvert(temperature, convert) {
+function tryConvert(temperature, funcConvert) {
   const input = parseFloat(temperature);
   if (Number.isNaN(input)) {
     return "";
   }
-  const output = convert(input);
+  const output = funcConvert(input);
   const rounded = Math.round(output * 1000) / 1000;
   return rounded.toString();
 }
